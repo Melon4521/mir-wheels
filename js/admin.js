cfe57efa52e147698f35d4fa8dadcaadd960174d
vlad <vlad123kyz@gmail.com>
2022-12-29 13:14:07 +0300

    Admin part v1.0 - 29/12/22

--- js/admin.js
+++ js/admin.js
@@ -1,8 +1,8 @@
-let root = document.querySelector("section");
+let root = document.querySelector("#root");
 
 function render() {
-    root.innerHTML = /*html*/ `
-    <div id='upload'>
+    /*
+    `<div id='upload'>
         <form method="post" enctype="multipart/form-data">
             <!--enctype="multipart/form-data" чтобы передовалось не только имя файла но и его содержимое-->
             <div class='title'>
@@ -22,42 +22,132 @@ function render() {
             <button type="submit">Конвертация и выгрузка в базу</button>
         </form>
     </div>
-  `;
+    `;
+    */
+
+    root.innerHTML = /*html*/`
+    <div class="wrapper">
+
+        <!-- HEADER -->
+        <header class="header lock-padding">
+            <div class="header__container top-header">
+                <div class="top-header__logo header-logo">
+                    <img class="header-logo__img" width="70" src="../images/Logo-2.png" alt="Логотип">
+                    <div class="logo__text">
+                        <h3 class="header-logo__title">Мир колёс</h3>
+                        <h4 class="header-logo__subtitle">шины и диски</h4>
+                    </div>
+                </div>
+                <div class="top-header__menu top-menu">
+                    <a class="top-menu__tel" href="http://mir-wheels.ru">Вернуться на сайт</a>
+                </div>
+            </div>
+        </header>
+        
+        <!-- MAIN -->
+        <main class="main">
+
+            <div class="load">
+                <div class="load__container">
+                    <form class="load__form" method="post" enctype="multipart/form-data">
+                    
+                        <h1 class="load__title _title">Загрузка данных на сайт</h1>
+                        <div class="load__conv conv">
+                            <div class="conv__item">
+                                <h3 class="conv__title">Шины</h3>
+                                <input class="conv__input" id="tires" type="file" name="uploaded_file_tire">
+                            </div>
+                            <div class="conv__item">
+                                <h3 class="conv__title">Диски</h3>
+                                <input class="conv__input" id="disks" type="file" name="uploaded_file_disk">
+                            </div>
+                        </div>
+                        <div class="load__finish">
+                            <button class="load__button _button block" type="submit">Конвертация и выгрузка в базу</button>
+                        </div>
+
+                    </form>
+                </div>
+            </div>
+
+        </main>
+        
+        <!-- FOOTER -->
+        <footer class="footer"></footer>
+
+    </div>
+    `
+
+    let inputButton = root.querySelector('.load__button'),
+        inputs = root.querySelectorAll('.conv__input'),
+        counter = 0;
+    
+    inputs.forEach(input => {
+        input.addEventListener('change', function () {
+            if (this.value) {
+                counter++;
+                console.log(counter);
+                if (counter >= 2) {
+                    if (inputButton.classList.contains('block')) {
+                        inputButton.classList.remove('block')
+                    }
+                }
+            }
+        });
+    });
 }
 
-document.getElementById("formregister").addEventListener("submit", (event) => {
-    event.preventDefault();
-});
-
-String.prototype.hashCode = function () {
-    var hash = 0,
-        i,
-        chr;
-    if (this.length === 0) return hash;
-    for (i = 0; i < this.length; i++) {
-        chr = this.charCodeAt(i);
-        hash = (hash << 100) - hash + chr;
-        hash |= 0; // Convert to 32bit integer
-    }
-    return hash;
-};
-
-root.addEventListener("keyup", () => {
-    if (
-        document.querySelector("#login").value &&
-        document.querySelector("#password").value
-    ) {
-        document.querySelector("#egsit").classList.remove("block");
-    } else {
-        document.querySelector("#egsit").classList.add("block");
-    }
-});
-
-document.querySelector("#egsit").addEventListener("click", (e) => {
-    if (
-        document.querySelector("#login").value.hashCode() == "6254155" &&
-        document.querySelector("#password").value.hashCode() == "-367044297"
-    ) {
-        render();
-    }
-});
+if (root.innerHTML != /*html*/`
+    <!-- HEADER -->
+    <header class="header lock-padding">
+        <div class="header__container top-header">
+            <div class="top-header__logo header-logo">
+                <img class="header-logo__img" width="70" src="../images/Logo-2.png" alt="Логотип">
+                <div class="logo__text">
+                    <h3 class="header-logo__title">Мир колёс</h3>
+                    <h4 class="header-logo__subtitle">шины и диски</h4>
+                </div>
+            </div>
+            <div class="top-header__menu top-menu">
+                <a class="top-menu__tel" href="http://mir-wheels.ru">Вернуться на сайт</a>
+            </div>
+        </div>
+    </header>
+`) {
+    document.getElementById("formregister").addEventListener("submit", (event) => {
+        event.preventDefault();
+    });
+    
+    String.prototype.hashCode = function () {
+        var hash = 0,
+            i,
+            chr;
+        if (this.length === 0) return hash;
+        for (i = 0; i < this.length; i++) {
+            chr = this.charCodeAt(i);
+            hash = (hash << 100) - hash + chr;
+            hash |= 0; // Convert to 32bit integer
+        }
+        return hash;
+    };
+    
+    root.addEventListener("keyup", () => {
+        if (
+            document.querySelector("#login").value &&
+            document.querySelector("#password").value
+        ) {
+            document.querySelector("#egsit").classList.remove("block");
+        } else {
+            document.querySelector("#egsit").classList.add("block");
+        }
+    });
+    
+    document.querySelector("#egsit").addEventListener("click", (e) => {
+        if (
+            document.querySelector("#login").value.hashCode() == "6254155" &&
+            document.querySelector("#password").value.hashCode() == "-367044297"
+        ) {
+            render();
+        }
+    });
+}
--- php/load_conv.php
+++ php/load_conv.php
@@ -6,225 +6,252 @@
 	<meta content="IE=edge,chrome=1" http-equiv="X-UA-Compatible">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 	<title>Страница Администрирования</title>
-	<link href="/dashboard/images/favicon.png" rel="icon" type="image/png" />
+	<link href="images/Logo-2.png" rel="icon" type="image/png"/>
 
 	<link rel="stylesheet" href="../css/admin.css">
 </head>
 
-<body style="padding: 15px;">
+<body>
 
-	<header>
-		<div class='logo'>
-			<div>
-				<img src="../Logo-2.png">
-			</div>
-			<div class='title'>
-				<div>Мир колёс</div>
-				<div>шины и диски</div>
-			</div>
-		</div>
+	<div id="root">
 
-		<h3 style="margin-left: 40px">
-			Страница администрирования
-		</h3>
-	</header>
+		<div class="wrapper">
 
-	<section>
-		<div id='register'>
-			<form id='formregister'>
-				<div class='top'>
-					Авторизация
-				</div>
-				<div class='body'>
-					<div class='title'>
-						Ваши данные
+			<!-- HEADER -->
+			<header class="header lock-padding">
+				<div class="header__container top-header">
+					<div class="top-header__logo header-logo">
+						<img class="header-logo__img" width="70" src="../images/Logo-2.png" alt="Логотип">
+						<div class="logo__text">
+							<h3 class="header-logo__title">Мир колёс</h3>
+							<h4 class="header-logo__subtitle">шины и диски</h4>
+						</div>
 					</div>
-					<div>
-						<input id='login' type='text' placeholder='Логин' required />
-					</div>
-					<div>
-						<input id='password' type='password' placeholder='Пароль' required />
+					<div class="top-header__menu top-menu">
+						<a class="top-menu__tel" href="http://mir-wheels.ru">Вернуться на сайт</a>
 					</div>
 				</div>
-				<input class='block' id='egsit' type='submit' value='Войти' />
-			</form>
+			</header>
+
+			<!-- MAIN -->
+			<main class="main">
+
+				<div class="reg">
+					<div class="reg__container">
+						<h1 class="reg__title _title">Авторизация</h1>
+						<div class="reg__auth auth">
+							<h3 class="auth__title">Ваши данные</h3>
+							<form class="auth__form" id="formregister">
+								<input class="auth__input" id='login' type="text" placeholder="Логин" required>
+								<input class="auth__input" id='password' type="password" placeholder="Пароль" required>
+								<button class="auth__button _button" id='egsit' type="submit">Войти</button>
+							</form>
+						</div>
+					</div>
+				</div>
+
+			</main>
+
+			<!-- FOOTER -->
+			<footer class="footer"></footer>
+
 		</div>
-	</section>
 
-	<footer>
-		<script type='module' src='../js/admin.js'></script>
-	</footer>
+	</div>
 
-<?php
-	
-	$doConv =  false;
-	
-	echo "<br><br>";
-	echo "<br><br>";
-	echo "<br><br>";
+	<script type='module' src='../js/admin.js'></script>
 
-	
-	ini_set('memory_limit', '500M');
-	
-	if ($_FILES && $_FILES['uploaded_file_tire'] &&
-	$_FILES['uploaded_file_tire']['error'] == 0) {
-
-		$nameTires = $_FILES['uploaded_file_tire']['name'];
-		//	echo "<br>имя передаваемого файла = ".$nameTres."<br>";
- 		  		  
-		$tmpnameTires = $_FILES['uploaded_file_tire']['tmp_name'];
-		//	echo "<br>временное имя = ".$tmpnameTires."<br>";
-	  
-		$typeTires = $_FILES['uploaded_file_tire']['type'];
-		//	echo "<br>тип передаваемого файла = ".$typeTires."<br>";
-		  
-  		$filetypeTires  = mime_content_type($tmpnameTires );
-		//	echo "<br>Реальный тип передаваемого файла = ".$filetypeTires."<br>";
-		  
-		$errorTires  = $_FILES['uploaded_file_tire']['error'];
-		//	echo "<br>Есть ли ошибки при передачи = ".$errorTires ."<br>";
-		  
-		$sizeTires  = $_FILES['uploaded_file_tire']['size'];
-		//	echo "<br>размер передаваемого файла = ".$sizeTires ."<br>";
-
-		$newpath = 'converter/';
-		  		  
-		$fd = fopen($newpath."log.txt", 'a+') or die("не удалось создать файл");
-		  
-		if (true) {
-			$newnameTires = $nameTires;
+	<div class="output__container" style="text-align: center; padding-bottom: 50px; margin-top: 80px">
+	<?php
 		
-	 		if (move_uploaded_file($tmpnameTires , $newpath.$newnameTires )) {
-			//	echo 'Файл загружен <br> <img width=30% src="'.$newnameTires .' ">';
-				$str = date("HisdmY")."-".$newnameTires."\r\n";
-			} else {
-				echo'Файл не загружен';
-				echo "<br><br>";
-			}
+		$doConv =  false;
+		
+		ini_set('memory_limit', '500M');
+		
+		if ($_FILES && $_FILES['uploaded_file_tire'] &&
+		$_FILES['uploaded_file_tire']['error'] == 0) {
 
-			fwrite($fd, $str);
+			$nameTires = $_FILES['uploaded_file_tire']['name'];
+			//	echo "<br>имя передаваемого файла = ".$nameTres."<br>";
+					
+			$tmpnameTires = $_FILES['uploaded_file_tire']['tmp_name'];
+			//	echo "<br>временное имя = ".$tmpnameTires."<br>";
+		
+			$typeTires = $_FILES['uploaded_file_tire']['type'];
+			//	echo "<br>тип передаваемого файла = ".$typeTires."<br>";
 			
-			$doConv = true;
-
-			echo "Шины - ".$nameTires;
-			echo "<br><br>";
+			$filetypeTires  = mime_content_type($tmpnameTires );
+			//	echo "<br>Реальный тип передаваемого файла = ".$filetypeTires."<br>";
 			
-		} else {
-		 	echo 'Неверный формат файла';
-			echo "<br><br>";
-		}
-		  
-		fclose($fd);	  
-	}
-	
-	if ($_FILES &&	$_FILES['uploaded_file_disk'] &&
-	$_FILES['uploaded_file_disk']['error'] == 0) {
+			$errorTires  = $_FILES['uploaded_file_tire']['error'];
+			//	echo "<br>Есть ли ошибки при передачи = ".$errorTires ."<br>";
+			
+			$sizeTires  = $_FILES['uploaded_file_tire']['size'];
+			//	echo "<br>размер передаваемого файла = ".$sizeTires ."<br>";
 
-		$nameDisks= $_FILES['uploaded_file_disk']['name'];
-		//	echo "<br>имя передаваемого файла = ".$nameDisks."<br>";
+			$newpath = 'converter/';
+					
+			$fd = fopen($newpath."log.txt", 'a+') or die("не удалось создать файл");
+			
+			if (true) {
+				$newnameTires = $nameTires;
+			
+				if (move_uploaded_file($tmpnameTires, $newpath.$newnameTires)) {
+				//	echo 'Файл загружен <br> <img width=30% src="'.$newnameTires .' ">';
+					$str = date("H:i:s d/m/Y")." - ".$newnameTires."\r\n";
+				} else {
+					echo 'Файл не загружен';
+					echo "<br><br>";
+				}
 
-		$tmpnameDisks = $_FILES['uploaded_file_disk']['tmp_name'];
-		//	echo "<br>временное имя = ".$tmpnameDisks."<br>";
-
-		$typeDisks = $_FILES['uploaded_file_disk']['type'];
-		//	echo "<br>тип передаваемого файла = ".$typeDisks."<br>";
-
-		$filetypeDisks  = mime_content_type($tmpnameDisks );
-		//	echo "<br>Реальный тип передаваемого файла = ".$filetypeDisks."<br>";
-
-		$errorDisks  = $_FILES['uploaded_file_disk']['error'];
-		//	echo "<br>Есть ли ошибки при передачи = ".$errorDisks ."<br>";
-
-		$sizeTires  = $_FILES['uploaded_file_disk']['size'];
-		//	echo "<br>размер передаваемого файла = ".$sizeDisks ."<br>";
+				fwrite($fd, $str);
 				
-		$newpath = 'converter/';
-		  		  
-		$fd = fopen($newpath."log.txt", 'a+') or die("не удалось создать файл");
-		  
-		if (true) {
-			$newnameDisks  = $nameDisks ;
-		  
-	 		if (move_uploaded_file($tmpnameDisks, $newpath.$newnameDisks )) {
-				//	echo 'Файл загружен <br> <img width=30% src="'.$newnameDisks .' ">';
-				$str = date("HisdmY")."-".$newnameDisks."\r\n";
+				$doConv = true;
+
+				echo "Шины - ".$nameTires;
+				echo "<br><br>";
+				
 			} else {
-				echo'Файл не загружен';
+				echo 'Неверный формат файла';
+				echo "<br><br>";
+			}
+			
+			fclose($fd);	  
+		}
+		
+		if ($_FILES &&	$_FILES['uploaded_file_disk'] &&
+		$_FILES['uploaded_file_disk']['error'] == 0) {
+
+			$nameDisks= $_FILES['uploaded_file_disk']['name'];
+			//	echo "<br>имя передаваемого файла = ".$nameDisks."<br>";
+
+			$tmpnameDisks = $_FILES['uploaded_file_disk']['tmp_name'];
+			//	echo "<br>временное имя = ".$tmpnameDisks."<br>";
+
+			$typeDisks = $_FILES['uploaded_file_disk']['type'];
+			//	echo "<br>тип передаваемого файла = ".$typeDisks."<br>";
+
+			$filetypeDisks  = mime_content_type($tmpnameDisks );
+			//	echo "<br>Реальный тип передаваемого файла = ".$filetypeDisks."<br>";
+
+			$errorDisks  = $_FILES['uploaded_file_disk']['error'];
+			//	echo "<br>Есть ли ошибки при передачи = ".$errorDisks ."<br>";
+
+			$sizeTires  = $_FILES['uploaded_file_disk']['size'];
+			//	echo "<br>размер передаваемого файла = ".$sizeDisks ."<br>";
+					
+			$newpath = 'converter/';
+					
+			$fd = fopen($newpath."log.txt", 'a+') or die("не удалось создать файл");
+			
+			if (true) {
+				$newnameDisks  = $nameDisks ;
+			
+				if (move_uploaded_file($tmpnameDisks, $newpath.$newnameDisks )) {
+					//	echo 'Файл загружен <br> <img width=30% src="'.$newnameDisks .' ">';
+					$str = date("H:i:s d/m/Y")." - ".$newnameDisks."\r\n";
+				} else {
+					echo'Файл не загружен';
+					echo "<br><br>";
+				}
+
+				fwrite($fd, $str);
+				$doConv = true;
+
+				echo "Диски - ".$nameDisks;
+				echo "<br><br>";
+				
+			} else {
+				echo 'Неверный формат файла';
 				echo "<br><br>";
 			}
 
-			fwrite($fd, $str);
-			$doConv=true;
-
-			echo "Диски - ".$nameDisks ;
-			echo "<br><br>";
-			
-		} else {
-			echo 'Неверный формат файла';
-			echo "<br><br>";
+			fclose($fd);	  
 		}
 
-		fclose($fd);	  
-	}
+		echo "<br><br>";
+		
+		if ($doConv) {
+			// Первая конвертация. Шины
+			$tiresXml = simplexml_load_file("../php/converter/".$nameTires);
+			$dataTires = json_encode($tiresXml, JSON_UNESCAPED_UNICODE); 
 
-	echo "<br><br>";
-	
-	if ($doConv) {
-	// Первая конвертация. Шины
-	$tiresXml = simplexml_load_file("../php/converter/".$nameTires);
-	$dataTires = json_encode($tiresXml, JSON_UNESCAPED_UNICODE); 
+			$tiresJsonFile = fopen("../api/tires-stage.json", "w");
+			fwrite($tiresJsonFile, $dataTires);
+			fclose($tiresJsonFile);
 
-	$tiresJsonFile = fopen("../api/tires-stage.json", "w");
-	fwrite($tiresJsonFile, $dataTires);
-	fclose($tiresJsonFile);
+			echo "<b>Конвертация Шин завершена!</b><br><br>";
 
-	echo "<b>Конвертация Шин завершена!</b><br><br>";
+			// Вторая конвертация. Диски
+			$disksXml = simplexml_load_file("../php/converter/".$nameDisks);
+			$dataDisks = json_encode($disksXml, JSON_UNESCAPED_UNICODE); 
 
-	// Вторая конвертация. Диски
-	$disksXml = simplexml_load_file("../php/converter/".$nameDisks);
-	$dataDisks = json_encode($disksXml, JSON_UNESCAPED_UNICODE); 
+			$disksJsonFile = fopen("../api/disks-stage.json", "w");
+			fwrite($disksJsonFile, $dataDisks);
+			fclose($disksJsonFile);
 
-	$disksJsonFile = fopen("../api/disks-stage.json", "w");
-	fwrite($disksJsonFile, $dataDisks);
-	fclose($disksJsonFile);
+			echo "<b>Конвертация Дисков завершена!</b><br><br>";
 
-	echo "<b>Конвертация Дисков завершена!</b><br><br>";
+			// Завершение работы
+			$jsonReadyTires = file_get_contents('../api/tires-stage.json', 'r');
+			$jsonReadyDisks = file_get_contents('../api/disks-stage.json', 'r');
 
-	// Завершение работы
-	$jsonReadyTires = file_get_contents('../api/tires-stage.json', 'r');
-	$jsonReadyDisks = file_get_contents('../api/disks-stage.json', 'r');
+			$tiresOffer = json_decode($jsonReadyTires, true)['shop']['offers']['offer'];
+			$disksOffer = json_decode($jsonReadyDisks, true)['shop']['offers']['offer'];
 
-	$tiresOffer = json_decode($jsonReadyTires, true)['shop']['offers']['offer'];
-	$disksOffer = json_decode($jsonReadyDisks, true)['shop']['offers']['offer'];
+			$tiresDataString = '{'.'"tires": '.json_encode($tiresOffer, JSON_UNESCAPED_UNICODE)."}";
+			$disksDataString = '{'.'"disks": '.json_encode($disksOffer, JSON_UNESCAPED_UNICODE)."}";
 
-	$tiresDataString = '{'.'"tires": '.json_encode($tiresOffer, JSON_UNESCAPED_UNICODE)."}";
-	$disksDataString = '{'.'"disks": '.json_encode($disksOffer, JSON_UNESCAPED_UNICODE)."}";
+			$tiresDataString = str_replace('\/', '/', $tiresDataString);
+			$disksDataString = str_replace('\/', '/', $disksDataString);
+			$tiresDataString = str_replace('[]', '""', $tiresDataString);
+			$disksDataString = str_replace('[]', '""', $disksDataString);
 
-	$tiresDataString = str_replace('\/', '/', $tiresDataString);
-	$disksDataString = str_replace('\/', '/', $disksDataString);
-	$tiresDataString = str_replace('[]', '""', $tiresDataString);
-	$disksDataString = str_replace('[]', '""', $disksDataString);
+			$newTiresJson = fopen('../api/tires-stage.json', 'w');
+			$newDisksJson = fopen('../api/disks-stage.json', 'w');
 
-	$newTiresJson = fopen('../api/tires-stage.json', 'w');
-	$newDisksJson = fopen('../api/disks-stage.json', 'w');
+			fwrite($newTiresJson, $tiresDataString);
+			fwrite($newDisksJson, $disksDataString);
 
-	fwrite($newTiresJson, $tiresDataString);
-	fwrite($newDisksJson, $disksDataString);
+			fclose($newTiresJson);
+			fclose($newDisksJson);
+					
+			unlink("../php/converter/".$nameTires);	
+			unlink("../php/converter/".$nameDisks);
 
-	fclose($newTiresJson);
-	fclose($newDisksJson);
-			
-	unlink("../php/converter/".$nameTires);	
-	unlink("../php/converter/".$nameDisks);
-	echo "<b>Временные файлы  шин и дисков для конвертации удалены с сервера!</b><br><br>";
-	echo "<b>Данные о загрузке внесены в журнал!</b><br><br>";
+			echo '<div class="checking"></div>';
+			echo "<b>Временные файлы  шин и дисков для конвертации удалены с сервера!</b><br><br>";
+			echo "<b>Данные о загрузке внесены в журнал!</b><br><br>";
 
-	echo "<br><br><b>Поздравляю, работа завершена!</b><br><br>";
-	echo	'<a href = "https://tires.intermir.ru">Перейти в магазин</a>';
+			echo "<br><br><b>Поздравляю, работа завершена!</b><br><br>";
+			echo '<a href = "http://mir-wheels.ru">Перейти в магазин</a>';
+		}
 
-	}
+	?>
+	</div>
 
-?>
+	<!-- Проверка, выполнен ли php-код-->
+	<script>
+		if (document.querySelector(".output__container").querySelector('.checking')) {
+    		document.querySelector("#root").innerHTML = /*html*/`
+				<!-- HEADER -->
+				<header class="header lock-padding">
+					<div class="header__container top-header">
+						<div class="top-header__logo header-logo">
+							<img class="header-logo__img" width="70" src="../images/Logo-2.png" alt="Логотип">
+							<div class="logo__text">
+								<h3 class="header-logo__title">Мир колёс</h3>
+								<h4 class="header-logo__subtitle">шины и диски</h4>
+							</div>
+						</div>
+						<div class="top-header__menu top-menu">
+							<a class="top-menu__tel" href="http://mir-wheels.ru">Вернуться на сайт</a>
+						</div>
+					</div>
+				</header>
+    		`;
+		}
+	</script>
 
 </body>
 </html>
