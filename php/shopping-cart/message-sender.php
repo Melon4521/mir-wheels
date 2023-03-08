8cadb16839c75aea145beddd5d3a85e2ffb640da
vlad <vlad123kyz@gmail.com>
2023-03-08 23:35:42 +0300

    test version 2.5

--- php/shopping-cart/message-sender.php
+++ php/shopping-cart/message-sender.php
@@ -14,64 +14,64 @@
 // if ($error) {
 //     echo 'Ошибка заполнения капчи.';
 // } else {
-    /* Здесь проверяется существование переменных */
-    if (isset($_POST['name'])) {
-        $name = $_POST['name'];
-    }
+/* Здесь проверяется существование переменных */
+if (isset($_POST['name'])) {
+    $name = $_POST['name'];
+}
 
-    if (isset($_POST['phone'])) {
-        $phone = $_POST['phone'];
-    }
+if (isset($_POST['phone'])) {
+    $phone = $_POST['phone'];
+}
 
-    if (isset($_POST['mail'])) {
-        $emailCl = $_POST['mail'];
-    }
+if (isset($_POST['mail'])) {
+    $emailCl = $_POST['mail'];
+}
 
-    if (isset($_POST['cart'])) {
-        $cart = $_POST['cart'];
-    }
+if (isset($_POST['cart'])) {
+    $cart = $_POST['cart'];
+}
 
-    /* Сюда впишите свою эл. почту */
-    // $myaddress = "mirkoles61@yandex.ru, belozerov.o.s@yandex.ru";
-    $myaddress = "kuznetsov.neon-x@yandex.ru";
-    /* Сюда впишите эл. почту магазина */
-    $shopEmail = 'shop@mir-wheels.ru';
+/* Сюда впишите свою эл. почту */
+// $myaddress = "mirkoles61@yandex.ru, belozerov.o.s@yandex.ru";
+$myaddress = "kuznetsov.neon-x@yandex.ru";
+/* Сюда впишите эл. почту магазина */
+$shopEmail = 'shop@mir-wheels.ru';
 
-    // Письмо в магазин с заказом
+// Письмо в магазин с заказом
 
-    /* А здесь прописывается текст сообщения, \n - перенос строки */
-    $mes1 = "Заказ в магазине mir-wheels.ru\n
-    ==========================================\n
-    Имя: $name\n
-    Телефон: $phone\n
-    E-mail: $emailCl\n
-    ==========================================\n
-    Данные корзины:\n
-    $cart
-    ";
+/* А здесь прописывается текст сообщения, \n - перенос строки */
+$mes1 = "Заказ в магазине mir-wheels.ru\n
+==========================================\n
+Имя: $name\n
+Телефон: $phone\n
+E-mail: $emailCl\n
+==========================================\n
+Данные корзины:\n
+$cart
+";
 
-    /* А эта функция как раз занимается отправкой письма на указанный вами email */
-    $sub = 'Order'; // тема
-    $send = mail($myaddress, $sub, $mes1, "Content-type:text/plain;  Reply-To:no-reply@****.ru; Content-Transfer-Encoding: utf-8; charset = UTF-8\r\nFrom:$shopEmail");
+/* А эта функция как раз занимается отправкой письма на указанный вами email */
+$sub = 'Order'; // тема
+$send = mail($myaddress, $sub, $mes1, "Content-type:text/plain;  Reply-To:no-reply@****.ru; Content-Transfer-Encoding: utf-8; charset = UTF-8\r\nFrom:$shopEmail");
 
-    // Письмо отправителю о заказе
-    
-    /* А здесь прописывается текст сообщения, \n - перенос строки */
-    $mes2 = "Вы отправили заказ в mir-wheels.ru\n
-    ==========================================\n
-    Имя: $name\n
-    Телефон: $phone\n
-    E-mail: $emailCl\n
-    ==========================================\n
-    Данные корзины:\n
-    $cart
-    ";
+// Письмо отправителю о заказе
 
-    /* А эта функция как раз занимается отправкой письма на указанный вами email */
-    $send = mail($emailCl, $sub, $mes2, "Content-type:text/plain; charset = UTF-8\r\nFrom:$shopEmail");
+/* А здесь прописывается текст сообщения, \n - перенос строки */
+$mes2 = "Вы отправили заказ в mir-wheels.ru\n
+==========================================\n
+Имя: $name\n
+Телефон: $phone\n
+E-mail: $emailCl\n
+==========================================\n
+Данные корзины:\n
+$cart
+";
 
-    ini_set('short_open_tag', 'On');
-    // header('Refresh: 5; URL = http://mir-wheels.ru');
+/* А эта функция как раз занимается отправкой письма на указанный вами email */
+$send = mail($emailCl, $sub, $mes2, "Content-type:text/plain; charset = UTF-8\r\nFrom:$shopEmail");
+
+ini_set('short_open_tag', 'On');
+// header('Refresh: 5; URL = http://mir-wheels.ru');
 // }
 
 ?>
